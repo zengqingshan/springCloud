e6fc1f11773fef36e7d43b132ba1c783611ac162
zengqingshan <1710759668@qq.com>
2020-02-11 19:47:48 +0800

    集合成Pom项目

---
+++ microservice-discovery-eureka/src/test/java/com/itmuch/cloud/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.microservicediscoveryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
