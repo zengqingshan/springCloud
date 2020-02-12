44a339b10b8bd4f4ce43769d8e52160d3ae8a93e
zengqingshan <1710759668@qq.com>
2020-02-12 23:02:34 +0800

    消费方和服务方都注册到eureka上

--- microservice-provider-user/src/main/java/com/itmuch/cloud/ProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/ProviderUserApplication.java
@@ -2,12 +2,15 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class MicroserviceSimpleProviderUserApplication {
+//@EnableEurekaClient
+@EnableDiscoveryClient
+public class ProviderUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleProviderUserApplication.class, args);
+		SpringApplication.run(ProviderUserApplication.class, args);
 	}
 
 }
--- microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.itmuch.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itmuch.cloud.entity.User;
@@ -10,12 +13,24 @@ import com.itmuch.cloud.repository.UserRepository;
 
 @RestController
 public class UserController {
+    /**
+     * 报错：Could not autowire. There is more than one bean of 'EurekaClient' type.
+     */
+//  @Autowired
+//  private EurekaClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private UserRepository userRepository;
 
-  @Autowired
-  private UserRepository userRepository;
+    @GetMapping("/simple/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userRepository.findOne(id);
+    }
 
-  @GetMapping("/simple/{id}")
-  public User findById(@PathVariable Long id) {
-    return this.userRepository.findOne(id);
-  }
+    @RequestMapping("/instance-info")
+    public ServiceInstance getServiceUrl() {
+        ServiceInstance serviceInstance = this.discoveryClient.getLocalServiceInstance();
+        return serviceInstance;
+    }
 }
