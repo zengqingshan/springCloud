fc736072a8fbc946926e02a8e695de653ea658ea
zengqingshan <1710759668@qq.com>
2020-02-13 14:22:25 +0800

    movice-ribbon-without-eureka

--- microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/itmuch/cloud/MovieRibbonWithoutEurekaApplication.java
+++ microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/itmuch/cloud/MovieRibbonWithoutEurekaApplication.java
@@ -1,18 +1,15 @@
 package com.itmuch.cloud;
 
-import com.itmuch.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "microservice-provider-user", configuration = RibbonConfiguration.class)
-public class ConsumerMovieRibbonApplication {
+public class MovieRibbonWithoutEurekaApplication {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
@@ -20,7 +17,7 @@ public class ConsumerMovieRibbonApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
+        SpringApplication.run(MovieRibbonWithoutEurekaApplication.class, args);
     }
 
 }
